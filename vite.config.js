85236f094ed5dafd72f8d538c29a0289882fa80a
Santi <scalvd01@estudiantes.unileon.es>
2023-10-11 01:46:26 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   assetsInclude: ['**/*.webp'],
+  build: { manifest: true },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
